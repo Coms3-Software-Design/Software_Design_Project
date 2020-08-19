ca30edd80a335152446bcf8edaa56366725b8e94
BraPhill <krbmadi@gmail.com>
2020-08-19 21:24:59 +0200

    Added the Homepage with it's CSS and .js files

---
+++ Scripts/Homepage.js
@@ -0,0 +1,63 @@
+let post = "posts";
+let user = "1814732";
+/*
+ * The 4 lines below gets information from the server
+ * We set the url and the array of categories
+ */
+const  url = 'https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/categories/categories.php';
+let productsArray;
+let goodsArray;
+let productHTML;
+
+var productBlock = document.getElementById("product-items");
+
+let categories = function() {
+    $.getJSON(url, function (result) {
+        goodsArray = result;
+        for (let i = 0; i < goodsArray.length; i++) {
+        	/*--------------- list of categories ----------------------*/
+            console.log(goodsArray[i].Category);
+
+            var heading = document.createElement("h1");
+            var node = document.createTextNode(goodsArray[i].Category);
+            heading.appendChild(node);
+            productBlock.appendChild(heading);
+
+            $.getJSON('https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/Products/products.php?&category='+ goodsArray[i].Category +'&type=Goods', function(results) {
+            	productsArray = results;
+            	for(let j = 0; j < productsArray.length; j++){
+            		console.log(productsArray[j].Product_Name);
+
+
+
+            		productHTML = '<div class="col-10 col-sm-8 col-lg-4 mx-auto my-3"> '+
+										'<div class="card single-item"> '+
+										    '<!-- Image of the product--> '+
+										    '<div class="img-container"> '+
+										        '<img src="../CSS/Images/iphone.jpg" alt="" class="card-img-top product-img"> '+
+										    '</div> '+
+										    '<div class="card-body"> '+
+										        '<div class="card-text d-flex justify-content-between text-capitalize"> '+
+										            '<h5 id="item-name"> '+
+										                productsArray[j].Product_Name+
+										            '</h5> '+
+										            '<span> '+
+										                '<i class="fas fa-dollar-sign"></i> '+
+										                'R' + productsArray[j].Product_Price+
+										            '</span> '+
+										        '</div> '+
+										    '</div> '+
+										'</div> '+
+									'</div>';
+
+					var productBLOCK = document.createElement("div");
+					productBLOCK.innerHTML = productHTML;
+		            var node = document.createTextNode(productHTML);
+		            productBlock.appendChild(productBLOCK);
+            	}
+            });
+        }
+    });
+};
+
+categories();
