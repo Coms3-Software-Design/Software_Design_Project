8e52ce6c6cdc35fcd008b2fc4b61957018f036f6
AmISerifSegoe <squareroot.broly88@gmail.com>
2020-08-22 17:06:07 +0200

    basic pop up

---
+++ Scripts/Login_popup.js
@@ -0,0 +1,8 @@
+$('body').on('click', '[data-toggle="modal"]', function(){
+    $($(this).data("target")+' .modal-body').load($(this).data("remote"));
+});     
+
+$('.li-modal').on('click', function(e){
+    e.preventDefault();
+    $('#theModal').modal('show').find('.modal-content').load($(this).attr('href'));
+  });
