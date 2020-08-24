cbae1a4e5864b5ba70574fe27ef0cb090ccefc2e
AmISerifSegoe <squareroot.broly88@gmail.com>
2020-08-24 14:52:41 +0200

    register connected to database

--- Scripts/Register.js
+++ Scripts/Register.js
@@ -1,17 +1,35 @@
-function validate(evt) {
-  var theEvent = evt || window.event;
+$(document).ready(function(){
+     $('#submit').click(function(e){
+       e.preventDefault();
+       var username = $("#username").val();
+       var stno = $("#stno").val();
+       var full_name = $("#full_name").val();
+       var last_name = $("#last_name").val();
+       var password = $("#password").val();
+       var phone = $("#phone").val();
+          if(username =='' || stno=='' || full_name== '' || last_name=='' || password=='' || phone=='')
+          {
+               $('#response').html('<span class="text-danger">All Fields are required</span>');
+          }
+          else
+          {
 
-  // Handle paste
-  if (theEvent.type === 'paste') {
-      key = event.clipboardData.getData('text/plain');
-  } else {
-  // Handle key press
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
-  }
-  var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
-    theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
-  }
-}
+               $.ajax({
+                    url:"https://lamp.ms.wits.ac.za/~s1814731/R.php",
+                    method:"POST",
+                    data:$('#submit_form').serialize(),
+                    dataType: "text",
+                    beforeSend:function(){
+                         $('#response').html('<span class="text-info">Loading response...</span>');
+                    },
+                    success:function(data){
+                         $('form').trigger("reset");
+                         $('#response').fadeIn().html(data);
+                         setTimeout(function(){
+                              $('#response').fadeOut("slow");
+                         }, 5000);
+                    }
+               });
+          }
+     });
+});
