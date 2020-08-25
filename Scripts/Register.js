01bce21fa081427b0752a293bae4ddda008aa9b9
raj7861 <Sirajmotaung@gmail.com>
2020-08-25 02:45:23 +0200

    User is now able to register

--- Scripts/Register.js
+++ Scripts/Register.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
        var username = $("#username").val();
        var stno = $("#userid").val();
        var full_name = $("#name").val();
-       var last_name = $("#name").val();
+       var last_name = $("#surname").val();
        var password = $("#password").val();
        var phone = $("#contactnum").val();
           if(username =='' || stno=='' || full_name== '' || last_name=='' || password=='' || phone=='')
@@ -15,12 +15,12 @@ $(document).ready(function(){
           {
 
                $.ajax({
-                    url:"https://lamp.ms.wits.ac.za/~s1814731/public_html/MPphpfiles/MPRegister.php",
+                    url:"https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPRegister.php",
                     method:"POST",
                     data:$('#submit_form').serialize(),
                     dataType: "text",
                     beforeSend:function(){
-                         $('#response').html('<span class="text-info">Loading response...</span>');
+                         $('#response').html('<span class="li-modal">Loading response...</span>');
                     },
                     success:function(data){
                          $('form').trigger("reset");
