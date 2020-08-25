4fa4988f3ba043c6f629f323406f7f1fe86d4708
raj7861 <Sirajmotaung@gmail.com>
2020-08-25 10:00:25 +0200

    User is now able to login

--- Scripts/Userlogin.js
+++ Scripts/Userlogin.js
@@ -1,29 +1,49 @@
 $(document).ready(function(){
-  $("#submit").click(function(){
+  $("#submit").click(function(e){
+    e.preventDefault();
     var username = $("#username").val();
     var password = $("#password").val();
 
-    if(username=="" || password==""){
-      alert("All feilds required");
-    }else{
-      $.ajax(
-        {
-            url:"https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php",
-            data:{
-              login:1,
-              username:username,
-              password:password
-            },
-            success:function(response){
-              alert("Login successfull");
-              console.log(response);
-            },
-            dataType: 'text'
-
+    url = 'https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php';
+    $.post(url,{username:username, password:password})
+    .done(function(data){
+        if(data!= "!exits"){
+          window.location = "home.html";
+        }else{
+          $("#login-message").text("Invalid username or password.");
+          $("#submit").show();
         }
-      );
 
-    }
+    });
+
+    // if(username=="" || password==""){
+    //   alert("All feilds required");
+    // }
+    // else{
+    //   $.ajax(
+    //     {
+    //         url:"https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php",
+    //         type: 'POST',
+    //         data:{
+    //           login:1,
+    //           username:username,
+    //           password:password
+    //         },
+    //
+    //         beforeSend:function(response){
+    //
+    //         },
+    //
+    //         success:function(response){
+    //           console.log(response);
+    //         },
+    //         dataType: 'text'
+    //
+    //
+    //     }
+    //   );
+    //
+    // }
 
   });
 });
