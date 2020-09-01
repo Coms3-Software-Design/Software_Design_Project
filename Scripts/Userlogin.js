2738167c0efe6c90ac485377ff51983502c8bbe1
Siraj <Sirajmotaung@gmail.com>
2020-09-01 19:34:49 +0200

    I added a css attribute to change color of the error message to red

--- Scripts/Userlogin.js
+++ Scripts/Userlogin.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
                 },50000);
               }
               else{
-
+                    
               }
             },
             dataType: 'text'
