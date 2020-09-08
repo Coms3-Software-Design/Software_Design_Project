e474b4d979974839a9d9b766d5cc1767a4cc2a94
Siraj <Sirajmotaung@gmail.com>
2020-09-08 11:41:10 +0200

    All fixed including session start

--- Scripts/Userlogin.js
+++ Scripts/Userlogin.js
@@ -1,38 +1,43 @@
 
-var button = document.getElementById('submit');
+let button = document.getElementById('submit');
 
 
 
-button.onclick = function(e){
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-
-
+button.addEventListener('click',function(e){
+e.preventDefault();
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;
 url = "https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php";
+
 
 $.getJSON(url,{username: username, password:password},function(results){
 
+//if the user inputs wrong credentials
+if(results=="!exists" && username!=""&&password!=""){
+  console.log(results);
+    $("#response").fadeIn().html('<span>Invalid username or password</span>');
+    setTimeout(function(){$("#response").fadeOut('slow');},5000);
+    return;
 
-  if(username==""||password==""){
-  e.preventDefault();
+  }
+  //If the user exists in the database then it links to the Homepage
+  else if( results!="!exists" && username!="" && password!=""){
+
+    sessionStorage.setItem('user', JSON.stringify(results[0]));
+    console.log(JSON.parse(sessionStorage.getItem('user')));
+    window.location.href='Homepage.html';
+  }
+  //When the fields are empty
+  else{
+
   $("#response").fadeIn().html('<span>All fields required</span>');
   setTimeout(function(){ $("#response").fadeOut('slow');}, 3000);
   return;
 }
-else if(results=="!exists"){
-    e.preventDefault();
-    $("#response").fadeIn().html('<span>Invalid username or password</span>');
-    setTimeout(function(){$("#response").fadeOut('slow');},5000);
-  }
-
-
-
-
-console.log(results);
 
 });
 
 
 
 
-};
+});
