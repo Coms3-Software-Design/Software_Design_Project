81f4c3e7c025e460a16c150595eea3cf2b7d4604
Beasty786 <shameelnkosi@gmail.com>
2020-09-08 09:25:14 +0200

    fixed the login return JSON array

--- Scripts/Userlogin.js
+++ Scripts/Userlogin.js
@@ -1,16 +1,18 @@
 
-var button = document.getElementById('submit');
-var username = document.getElementById('username');
-var password = document.getElementById('password');
+let button = document.getElementById('submit');
 
 
-button.onclick = function(){
-//alert(username);
+
+button.onclick = function(e){
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+e.preventDefault();
 
 url = "https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php";
-$.getJSON(url,{username: "Bee", password:"1234555"},function(results){
+$.getJSON(url,{username: username, password:password},function(results){
 
-console.log("beee");
+console.log(results);
 
 });
 
