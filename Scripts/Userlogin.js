ae40c9bc211f9c63179bf08a8684afd836604be6
Siraj <Sirajmotaung@gmail.com>
2020-09-08 10:08:23 +0200

    Able to check if user exists in the databasegit pull

--- Scripts/Userlogin.js
+++ Scripts/Userlogin.js
@@ -1,16 +1,32 @@
 
-let button = document.getElementById('submit');
+var button = document.getElementById('submit');
 
 
 
 button.onclick = function(e){
-  let username = document.getElementById('username').value;
-  let password = document.getElementById('password').value;
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
 
-e.preventDefault();
 
 url = "https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPLogin.php";
+
 $.getJSON(url,{username: username, password:password},function(results){
+
+
+  if(username==""||password==""){
+  e.preventDefault();
+  $("#response").fadeIn().html('<span>All fields required</span>');
+  setTimeout(function(){ $("#response").fadeOut('slow');}, 3000);
+  return;
+}
+else if(results=="!exists"){
+    e.preventDefault();
+    $("#response").fadeIn().html('<span>Invalid username or password</span>');
+    setTimeout(function(){$("#response").fadeOut('slow');},5000);
+  }
+
+
+
 
 console.log(results);
 
