b3f710d87c69599b774605ffe4b9d7718be82b08
Siraj <Sirajmotaung@gmail.com>
2020-09-14 22:32:49 +0200

    Viewing profile works

--- Scripts/Viewprofile.js
+++ Scripts/Viewprofile.js
@@ -1,3 +1,26 @@
 
-var user = JSON.parse(sessionStorage.getItem('user'));
+let user = JSON.parse(sessionStorage.getItem('user'));
 console.log(user);
+const picURL = 'https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/uploads/';
+
+var UserId = user.UserID;
+var Name = user.Name;
+var Surname = user.Surname;
+var UserName = user.UserName;
+var Password = user.Password;
+var ContactDetails = user.ContactNum;
+var DateOfBirth = user.D_O_B;
+var DateCreated = user.Date_Created;
+var Gender = user.Gender;
+var Bio = user.Bio;
+var Balance = user.Balance;
+var Profilepic = user.Profilepic;
+
+var abuser = new User(UserId,Name,Surname,UserName,Password,ContactDetails,DateOfBirth,DateCreated,Gender,Bio,Balance,Profilepic);
+console.log(user.UserName);
+$("#p-Name").html(`<p class="pItem">${abuser.getName()}</p>`);
+$("#p-surname").html(`<p class="pItem">${Surname}</p>`);
+$("#p-userName").html(`<p class="pItem">${user.UserName}</p>`);
+$("#p-Bio").html(`<p class="pItem">${Bio}</p>`);
+$("#p-No").html(`<p class="pItem">${ContactDetails}</p>`);
+$("#p-Propic").html(`<img src="${picURL}${UserId}.jpg" alt="../CSS/Images/iphone.jpg" id="profilePic">`);
