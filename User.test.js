9a619e45c1fd13403c4ee2a6826ec423d816ffca
Mahlatse-27 <millieniummorapedi0212@gmail.com>
2020-09-15 12:42:47 +0200

    Working noma qha

--- User.test.js
+++ User.test.js
@@ -15,3 +15,15 @@ test("Returning information of the user", () => {
 	expect(user.contactDetails).toBe('0860010111');
 	expect(user.proPicURL).toBe('www.hello.com/images/image.png')
 });
+
+test('get functions', ()=>{
+	const user = new User('101', 'Billy', 'McTominay', 'BigTee', 'BigMacTeaMoney', '0860010111', '22-06-2010', '30-06-2017', 'male', 'low-key douche', '5000', 'www.hello.com/images/image.png');
+	
+	const spy = jest.spyOn(product, 'getUserID', 'get');
+	const  getterUserID = product.getUserID;
+
+	expect(spy).toHaveBeenCalled();
+	expect(getterUserID).toBe('101');
+
+	spy.mockRestore();
+});
