3f6ef67e59a06776b13b9670505a590b3f0ba28e
Siraj <Sirajmotaung@gmail.com>
2020-09-29 13:46:24 +0200

    switching

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-
-require('dotenv').config();
+require("dotenv").config();
 
 const nodemailer = require("nodemailer");
 
@@ -10,9 +9,9 @@ let transporter = nodemailer.createTransport({
   //domain
   service: "gmail",
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
+    user: "info.sibawaih@gmail.com",
+    pass: "2443"
+  }
 });
 
 //step 2.
@@ -22,14 +21,14 @@ let mailOptions = {
   from: "info.sibawaih@gmail.com",
   to: "Sirajmotaung@gmail.com",
   subject: "Testing and Testing",
-  text: "It works",
+  text: "It works"
 };
 
 //Step 3
 
 transporter.sendMail(mailOptions, function (err, data) {
   if (err) {
-    console.log("Error occurs");
+    console.log("Error occurs", err);
   } else {
     console.log("Email sent!!!");
   }
