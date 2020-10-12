4d27b9b58c8ae80711c11b4b14892d52a3782d8a
Beasty786 <shameelnkosi@gmail.com>
2020-10-12 20:28:42 +0200

    Three files added

--- Scripts/Register.js
+++ Scripts/Register.js
@@ -1,3 +1,70 @@
+
+const registerURL = "https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPRegister.php";
+
+
+let dateCreated = new Date();
+let dd = String(dateCreated.getDate()).padStart(2, '0');
+let mm = String(dateCreated.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = dateCreated.getFullYear();
+dateCreated = dd + '-' + mm + '-' + yyyy;
+
+let Button = document.getElementById("submit");
+
+Button.addEventListener('click', function(e){
+    e.preventDefault();
+     var username = document.getElementById("username").value;
+     var stno = document.getElementById("userid").value;
+     var full_name = document.getElementById("name").value;
+     var last_name = document.getElementById("surname").value;
+     var password = document.getElementById("password").value;
+     var contact = document.getElementById("contactnum").value;
+     let gender = document.getElementById("male").checked?"Male":"Female";
+     let dob = document.getElementById("dob").value;
+
+     
+     
+     if(username =='' || stno=='' || full_name== '' || last_name=='' || password=='' || contact=='' || dob == '')
+     {
+          $('#response').html('<span class="text-danger">All Fields are required</span>');
+          return;
+     }
+
+     var patterns = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+     if(!contact.match(patterns))
+     {
+         alert("Invalid Email Address");  
+         return;   
+     }
+
+     $.getJSON(registerURL , {
+          userid : stno,
+          name : full_name,
+          surname : last_name,
+          username : username,
+          password : password,
+          contactnum : contact,
+          datecreated : dateCreated,
+          dob : dob,
+          gender : gender
+     }, (response) => {
+          $('form').trigger("reset");
+          $('#response').fadeIn().html('<span class="li-modal">registration successfull</span>');
+          
+          let prom = new Promise(resolve =>{
+               setTimeout(function(){
+                    $('#response').fadeOut("slow");
+                    resolve();
+               }, 5000);
+          })
+          
+          prom.then(()=>{
+               window.location.href = 'Login.js';
+          })
+          
+     });
+});
+
+/*
 $(document).ready(function(){
      $('#submit').click(function(e){
 
@@ -8,6 +75,7 @@ $(document).ready(function(){
        var last_name = $("#surname").val();
        var password = $("#password").val();
        var phone = $("#contactnum").val();
+       
         //$('#datecreated').val(new Date().toDateInputValue());
 
 
@@ -40,3 +108,5 @@ $(document).ready(function(){
           }
      });
 });
+*/
+
