a1da5c557e811db574a9cffedd3adc77497cf43f
Siraj <Sirajmotaung@gmail.com>
2020-10-13 15:59:02 +0200

    Hallo

--- Scripts/change.js
+++ Scripts/change.js
@@ -1,9 +1,12 @@
 
 const changUrl = 'https://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/MPReset.php';
 
+let Username = document.getElementById('');
+let pass = document.getElementById('');
+
 $.getJSON(changUrl, {
-    UserID: users.UserID,
-    new_pass: currentpass,
+    UserID: Username,
+    new_pass: pass,
 }, result => {
     if (result == "0") {
         alert("Change Password Failed")
